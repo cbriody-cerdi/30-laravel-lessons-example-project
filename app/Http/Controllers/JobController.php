0591b8b4141dc1f5ad8f6874763126ec62a017f8
Cbriody <c.briody@federation.edu.au>
2026-05-20 13:22:39 +1000

    End of lesson 19 files - Using GitHub Copilot to repair code warnings

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -1,15 +1,13 @@
 <?php
 
     namespace App\Http\Controllers;
-
     use App\Models\Job;
-    use Illuminate\Http\Request;
 
     class JobController extends Controller
     {
         public function index()
         {
-            $jobs = Job::with('employer')->latest()->Paginate(3);
+            $jobs = Job::with('employer')->latest()->paginate(3);
             return view('jobs.index', [
                 'jobs' => $jobs
             ]);
